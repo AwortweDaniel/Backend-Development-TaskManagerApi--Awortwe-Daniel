19feb79affb806bbea83680e14832dcc7bfa7438
Awortwe Daniel <123452581+AwortweDaniel@users.noreply.github.com>
2026-09-23 12:31:06 +0000

    add SQLite and sample task data

--- TaskManagerApi/Data/AppDbContext.cs
+++ TaskManagerApi/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using TaskManagerApi.Models;
 
 namespace TaskManagerApi.Data;
 
-public class AppDbContext: DbContext
+public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
