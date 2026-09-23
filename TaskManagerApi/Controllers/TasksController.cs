25b1ea251bc724a4e7f14ed2fd1c327b9bc324d7
Awortwe Daniel <123452581+AwortweDaniel@users.noreply.github.com>
2026-09-23 12:36:35 +0000

    implement task CRUD endpoints- controller

--- TaskManagerApi/Controllers/TasksController.cs
+++ TaskManagerApi/Controllers/TasksController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskManagerApi.Data;
 using TaskManagerApi.Dtos;
+
 using TaskManagerApi.Models;
 
 namespace TaskManagerApi.Controllers;
